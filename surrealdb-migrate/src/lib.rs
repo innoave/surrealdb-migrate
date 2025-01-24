ff1455fc8847bc8e5358a9e0ca58aa4ee3cd8fd4
haraldmaida <harald.maida@innoave.com>
2025-01-24 19:36:10 +0100

    re-export crates of project from the `surrealdb-migrate` crate for a one-crate-dependency experience

--- surrealdb-migrate/src/lib.rs
+++ surrealdb-migrate/src/lib.rs
@@ -1,7 +1,19 @@
 pub mod runner;
 
+//
+// re-export other crates of the project for a one-crate-dependency experience
+// for users of this crate.
+//
+
+pub use database_migration::*;
+pub use database_migration_files as files;
 #[cfg(feature = "config")]
 pub use surrealdb_migrate_config as settings;
+pub mod db_client {
+    pub use surrealdb_migrate_db_client::connect_to_database;
+    pub use surrealdb_migrate_db_client::DbConnection;
+    pub use surrealdb_migrate_db_client::DbError;
+}
 
 // workaround for false positive 'unused extern crate' warnings until
 // Rust issue [#95513](https://github.com/rust-lang/rust/issues/95513) is fixed
