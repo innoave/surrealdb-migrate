1fa6a6724c7889f737435fed225dc0c47e64d772
haraldmaida <harald.maida@innoave.com>
2025-01-31 20:40:46 +0100

    support all combinations of options for list command

--- surrealdb-migrate-cli/src/list_cmd.rs
+++ surrealdb-migrate-cli/src/list_cmd.rs
@@ -2,7 +2,7 @@ use crate::args::ListArgs;
 use crate::runner::runner;
 use crate::tables::format_migration_table;
 use color_eyre::Report;
-use std::cmp::Reverse;
+use std::cmp::{Ordering, Reverse};
 use surrealdb_migrate::config::RunnerConfig;
 use surrealdb_migrate::db_client::DbConnection;
 use surrealdb_migrate::migration::MigrationKind;
@@ -12,92 +12,230 @@ pub async fn run(
     config: RunnerConfig<'_>,
     db: &DbConnection,
 ) -> Result<(), Report> {
-    let mut no_records = None;
     let runner = runner(config);
-    let kind_criteria = if args.down && !args.up {
-        MigrationKind::is_backward
-    } else {
-        MigrationKind::is_forward
+    let kind_criteria = match (args.up, args.down) {
+        (_, false) => MigrationKind::is_forward,
+        (false, true) => MigrationKind::is_backward,
+        (true, true) => MigrationKind::is_any,
     };
     let migrations = runner.list_defined_migrations(kind_criteria)?;
     if migrations.is_empty() {
-        no_records = Some("No migrations defined.");
-    }
-    let mut executions = runner.list_applied_migrations(db).await?;
-    let mut entries = Vec::with_capacity(migrations.len());
-    match (args.up, args.down, args.applied, args.open) {
-        (true, _, _, _) | (_, false, false, false) | (_, false, true, true) => {
-            for migration in migrations {
-                let execution = executions
-                    .iter()
-                    .position(|exe| exe.key == migration.key)
-                    .map(|pos| executions.remove(pos));
-                entries.push((migration, execution));
-            }
-            println!("\nList of migrations:");
-        },
-        (_, false, true, false) => {
-            for migration in migrations {
-                if let Some(execution) = executions
-                    .iter()
-                    .position(|exe| exe.key == migration.key)
-                    .map(|pos| executions.remove(pos))
-                {
-                    entries.push((migration, Some(execution)));
+        println!("\nList of migrations:");
+        let migration_table = format_migration_table(vec![])?;
+        println!("{migration_table}");
+        println!("  No migrations defined.\n");
+    } else {
+        let mut executions = runner.list_applied_migrations(db).await?;
+        let mut entries = Vec::with_capacity(migrations.len());
+
+        match (args.up, args.down, args.applied, args.open) {
+            (_, false, false, false) | (_, false, true, true) => {
+                for migration in migrations {
+                    let execution = executions
+                        .iter()
+                        .position(|exe| exe.key == migration.key)
+                        .map(|pos| executions.remove(pos));
+                    entries.push((migration, execution));
                 }
-            }
-            println!("\nList of applied migrations:");
-        },
-        (_, false, false, true) => {
-            for migration in migrations {
-                let applied = executions.iter().any(|exe| exe.key == migration.key);
-                if !applied {
-                    entries.push((migration, None));
+                println!("\nList of migrations:");
+            },
+            (_, false, true, false) => {
+                for migration in migrations {
+                    if let Some(execution) = executions
+                        .iter()
+                        .position(|exe| exe.key == migration.key)
+                        .map(|pos| executions.remove(pos))
+                    {
+                        entries.push((migration, Some(execution)));
+                    }
                 }
-            }
-            println!("\nList of open migrations:");
-        },
-        (false, true, false, false) | (false, true, true, true) => {
-            for migration in migrations {
-                let execution = executions
-                    .iter()
-                    .position(|exe| exe.key == migration.key)
-                    .map(|pos| executions.remove(pos));
-                entries.push((migration, execution));
-            }
-            entries.sort_unstable_by_key(|(mig, _)| Reverse(mig.key));
-            println!("\nList of backward migrations:");
-        },
-        (false, true, true, false) => {
-            for migration in migrations {
-                let applied = executions.iter().any(|exe| exe.key == migration.key);
-                if !applied {
-                    entries.push((migration, None));
+                println!("\nList of applied migrations:");
+            },
+            (_, false, false, true) => {
+                for migration in migrations {
+                    let applied = executions.iter().any(|exe| exe.key == migration.key);
+                    if !applied {
+                        entries.push((migration, None));
+                    }
                 }
-            }
-            println!("\nList of applied backward migrations:");
-        },
-        (false, true, false, true) => {
-            for migration in migrations.into_iter().filter(|mig| mig.kind.is_backward()) {
-                if let Some(execution) = executions
-                    .iter()
-                    .position(|exe| exe.key == migration.key)
-                    .map(|pos| executions.remove(pos))
-                {
-                    entries.push((migration, Some(execution)));
+                println!("\nList of open migrations:");
+            },
+            (false, true, false, false) | (false, true, true, true) => {
+                for migration in migrations {
+                    let execution = executions
+                        .iter()
+                        .position(|exe| exe.key == migration.key)
+                        .map(|pos| executions.remove(pos));
+                    entries.push((migration, execution));
                 }
-            }
-            entries.sort_unstable_by_key(|(mig, _)| Reverse(mig.key));
-            println!("\nList of open backward migrations:");
-        },
-    }
-    if no_records.is_none() && entries.is_empty() {
-        no_records = Some("No migrations found for the specified options.");
-    }
-    let migration_table = format_migration_table(entries)?;
-    println!("{migration_table}");
-    if let Some(message) = no_records {
-        println!("  {message}\n");
+                entries.sort_unstable_by_key(|(mig, _)| Reverse(mig.key));
+                println!("\nList of backward migrations:");
+            },
+            (false, true, true, false) => {
+                for migration in migrations {
+                    let applied = executions.iter().any(|exe| exe.key == migration.key);
+                    if !applied {
+                        entries.push((migration, None));
+                    }
+                }
+                entries.sort_unstable_by_key(|(mig, _)| Reverse(mig.key));
+                println!("\nList of applied backward migrations:");
+            },
+            (false, true, false, true) => {
+                for migration in migrations.into_iter().filter(|mig| mig.kind.is_backward()) {
+                    if let Some(execution) = executions
+                        .iter()
+                        .position(|exe| exe.key == migration.key)
+                        .map(|pos| executions.remove(pos))
+                    {
+                        entries.push((migration, Some(execution)));
+                    }
+                }
+                entries.sort_unstable_by_key(|(mig, _)| Reverse(mig.key));
+                println!("\nList of open backward migrations:");
+            },
+            (true, true, false, false) | (true, true, true, true) => {
+                for migration in migrations {
+                    let execution = if migration.kind.is_forward() {
+                        executions
+                            .iter()
+                            .position(|exe| exe.key == migration.key)
+                            .map(|pos| executions.remove(pos))
+                    } else {
+                        None
+                    };
+                    entries.push((migration, execution));
+                }
+                entries.sort_unstable_by(|(mig1, _), (mig2, _)| {
+                    mig1.key
+                        .cmp(&mig2.key)
+                        .then_with(|| display_ordering(mig1.kind, mig2.kind))
+                });
+                println!("\nList of migrations:");
+            },
+            (true, true, false, true) => {
+                for migration in migrations {
+                    let execution = executions.iter().find(|exe| exe.key == migration.key);
+                    let applied = execution.is_some();
+                    if migration.kind.is_backward() == applied {
+                        let execution = execution.cloned();
+                        entries.push((migration, execution));
+                    }
+                }
+                entries.sort_unstable_by(|(mig1, _), (mig2, _)| {
+                    mig1.key
+                        .cmp(&mig2.key)
+                        .then_with(|| display_ordering(mig1.kind, mig2.kind))
+                });
+                println!("\nList of open migrations:");
+            },
+            (true, true, true, false) => {
+                for migration in migrations {
+                    let execution = executions.iter().find(|exe| exe.key == migration.key);
+                    let applied = execution.is_some();
+                    if migration.kind.is_forward() == applied {
+                        let execution = execution.cloned();
+                        entries.push((migration, execution));
+                    }
+                }
+                entries.sort_unstable_by(|(mig1, _), (mig2, _)| {
+                    mig1.key
+                        .cmp(&mig2.key)
+                        .then_with(|| display_ordering(mig1.kind, mig2.kind))
+                });
+                println!("\nList of applied migrations:");
+            },
+        }
+
+        let no_migrations_listed = entries.is_empty();
+        let migration_table = format_migration_table(entries)?;
+        println!("{migration_table}");
+        if no_migrations_listed {
+            println!("  No migrations found for the specified options.\n");
+        }
     }
     Ok(())
+}
+
+#[allow(clippy::enum_glob_use)]
+const fn display_ordering(mig1: MigrationKind, mig2: MigrationKind) -> Ordering {
+    use MigrationKind::*;
+    match (mig1, mig2) {
+        (Baseline, Baseline) | (Up, Up) | (Down, Down) => Ordering::Equal,
+        (Baseline, _) | (_, Down) => Ordering::Less,
+        (_, Baseline) | (Down, _) => Ordering::Greater,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assertor::*;
+
+    mod display_ordering {
+        use super::*;
+
+        #[test]
+        fn migration_kind_baseline_is_less_than_up() {
+            assert_that!(display_ordering(MigrationKind::Baseline, MigrationKind::Up))
+                .is_equal_to(Ordering::Less);
+        }
+
+        #[test]
+        fn migration_kind_baseline_is_less_than_down() {
+            assert_that!(display_ordering(
+                MigrationKind::Baseline,
+                MigrationKind::Down
+            ))
+            .is_equal_to(Ordering::Less);
+        }
+
+        #[test]
+        fn migration_kind_up_is_less_than_down() {
+            assert_that!(display_ordering(MigrationKind::Up, MigrationKind::Down))
+                .is_equal_to(Ordering::Less);
+        }
+
+        #[test]
+        fn migration_kind_up_is_greater_than_baseline() {
+            assert_that!(display_ordering(MigrationKind::Up, MigrationKind::Baseline))
+                .is_equal_to(Ordering::Greater);
+        }
+
+        #[test]
+        fn migration_kind_down_is_greater_than_baseline() {
+            assert_that!(display_ordering(
+                MigrationKind::Down,
+                MigrationKind::Baseline
+            ))
+            .is_equal_to(Ordering::Greater);
+        }
+
+        #[test]
+        fn migration_kind_down_is_greater_than_up() {
+            assert_that!(display_ordering(MigrationKind::Down, MigrationKind::Up))
+                .is_equal_to(Ordering::Greater);
+        }
+
+        #[test]
+        fn migration_kind_baseline_is_equal_baseline() {
+            assert_that!(display_ordering(
+                MigrationKind::Baseline,
+                MigrationKind::Baseline
+            ))
+            .is_equal_to(Ordering::Equal);
+        }
+
+        #[test]
+        fn migration_kind_up_is_equal_up() {
+            assert_that!(display_ordering(MigrationKind::Up, MigrationKind::Up))
+                .is_equal_to(Ordering::Equal);
+        }
+
+        #[test]
+        fn migration_kind_down_is_equal_down() {
+            assert_that!(display_ordering(MigrationKind::Down, MigrationKind::Down))
+                .is_equal_to(Ordering::Equal);
+        }
+    }
 }
--- surrealdb-migrate-cli/tests/list_cmd.rs
+++ surrealdb-migrate-cli/tests/list_cmd.rs
@@ -14,7 +14,7 @@ use surrealdb_migrate::test_dsl::{datetime, key};
 use surrealdb_migrate_db_client::insert_migration_execution;
 
 #[tokio::test]
-async fn list_migrations_non_applied() {
+async fn list_migrations_none_applied() {
     let db_server = start_surrealdb_testcontainer().await;
     let db_config = prepare_test_database(&db_server).await;
 
@@ -28,12 +28,12 @@ async fn list_migrations_non_applied() {
 
     cmd.assert()
         .code(0)
-        .stdout_eq(file!("list_cmd/forward_migrations_non_applied.stdout"))
+        .stdout_eq(file!("list_cmd/forward_migrations_none_applied.stdout"))
         .stderr_eq("");
 }
 
 #[tokio::test]
-async fn list_forward_migrations_non_applied() {
+async fn list_forward_migrations_none_applied() {
     let db_server = start_surrealdb_testcontainer().await;
     let db_config = prepare_test_database(&db_server).await;
 
@@ -48,7 +48,7 @@ async fn list_forward_migrations_non_applied() {
 
     cmd.assert()
         .code(0)
-        .stdout_eq(file!("list_cmd/forward_migrations_non_applied.stdout"))
+        .stdout_eq(file!("list_cmd/forward_migrations_none_applied.stdout"))
         .stderr_eq("");
 }
 
@@ -188,7 +188,7 @@ async fn list_applied_migrations_one_applied() {
 }
 
 #[tokio::test]
-async fn list_backward_migrations_non_applied() {
+async fn list_backward_migrations_none_applied() {
     let db_server = start_surrealdb_testcontainer().await;
     let db_config = prepare_test_database(&db_server).await;
 
@@ -203,7 +203,7 @@ async fn list_backward_migrations_non_applied() {
 
     cmd.assert()
         .code(0)
-        .stdout_eq(file!("list_cmd/backward_migrations_non_applied.stdout"))
+        .stdout_eq(file!("list_cmd/backward_migrations_none_applied.stdout"))
         .stderr_eq("");
 }
 
@@ -325,5 +325,174 @@ async fn list_migrations_in_empty_directory() {
     cmd.assert()
         .code(0)
         .stdout_eq(file!("list_cmd/in_empty_directory.stdout"))
+        .stderr_eq("");
+}
+
+#[tokio::test]
+async fn list_forward_and_backward_migrations_none_applied() {
+    let db_server = start_surrealdb_testcontainer().await;
+    let db_config = prepare_test_database(&db_server).await;
+
+    let cmd = surmig().args([
+        "--config-dir",
+        "tests/list_cmd",
+        "--db-address",
+        &db_config.address,
+        "list",
+        "--up",
+        "--down",
+    ]);
+
+    cmd.assert()
+        .code(0)
+        .stdout_eq(file!(
+            "list_cmd/forward_and_backward_migrations_none_applied.stdout"
+        ))
+        .stderr_eq("");
+}
+
+#[tokio::test]
+async fn list_forward_and_backward_migrations_one_applied() {
+    let db_server = start_surrealdb_testcontainer().await;
+    let db_config = prepare_test_database(&db_server).await;
+    let db = connect_to_test_database_as_database_user(&db_config).await;
+
+    let migration1 = Migration {
+        key: key("20250103_140520"),
+        title: "define quote table".into(),
+        kind: MigrationKind::Up,
+        script_path:
+            "../fixture/with_down_migrations/migrations/20250103_140520_define_quote_table.surql"
+                .into(),
+    };
+
+    let checksum1 = hash_migration_script(&migration1, "");
+
+    let execution1 = Execution {
+        key: key("20250103_140520"),
+        applied_rank: 1,
+        applied_by: "tester".into(),
+        applied_at: datetime("2025-01-20T09:10:19Z"),
+        checksum: checksum1,
+        execution_time: Duration::from_micros(256),
+    };
+
+    insert_migration_execution(migration1, execution1, DEFAULT_MIGRATIONS_TABLE, &db)
+        .await
+        .expect("failed to insert migration 1");
+
+    let cmd = surmig().args([
+        "--config-dir",
+        "tests/list_cmd",
+        "--db-address",
+        &db_config.address,
+        "list",
+        "--up",
+        "--down",
+    ]);
+
+    cmd.assert()
+        .code(0)
+        .stdout_eq(file!(
+            "list_cmd/forward_and_backward_migrations_one_applied.stdout"
+        ))
+        .stderr_eq("");
+}
+
+#[tokio::test]
+async fn list_open_forward_and_backward_migrations_one_applied() {
+    let db_server = start_surrealdb_testcontainer().await;
+    let db_config = prepare_test_database(&db_server).await;
+    let db = connect_to_test_database_as_database_user(&db_config).await;
+
+    let migration1 = Migration {
+        key: key("20250103_140520"),
+        title: "define quote table".into(),
+        kind: MigrationKind::Up,
+        script_path:
+            "../fixture/with_down_migrations/migrations/20250103_140520_define_quote_table.surql"
+                .into(),
+    };
+
+    let checksum1 = hash_migration_script(&migration1, "");
+
+    let execution1 = Execution {
+        key: key("20250103_140520"),
+        applied_rank: 1,
+        applied_by: "tester".into(),
+        applied_at: datetime("2025-01-20T09:10:19Z"),
+        checksum: checksum1,
+        execution_time: Duration::from_micros(256),
+    };
+
+    insert_migration_execution(migration1, execution1, DEFAULT_MIGRATIONS_TABLE, &db)
+        .await
+        .expect("failed to insert migration 1");
+
+    let cmd = surmig().args([
+        "--config-dir",
+        "tests/list_cmd",
+        "--db-address",
+        &db_config.address,
+        "list",
+        "--open",
+        "--up",
+        "--down",
+    ]);
+
+    cmd.assert()
+        .code(0)
+        .stdout_eq(file!(
+            "list_cmd/open_forward_and_backward_migrations_one_applied.stdout"
+        ))
+        .stderr_eq("");
+}
+
+#[tokio::test]
+async fn list_applied_forward_and_backward_migrations_one_applied() {
+    let db_server = start_surrealdb_testcontainer().await;
+    let db_config = prepare_test_database(&db_server).await;
+    let db = connect_to_test_database_as_database_user(&db_config).await;
+
+    let migration1 = Migration {
+        key: key("20250103_140520"),
+        title: "define quote table".into(),
+        kind: MigrationKind::Up,
+        script_path:
+            "../fixture/with_down_migrations/migrations/20250103_140520_define_quote_table.surql"
+                .into(),
+    };
+
+    let checksum1 = hash_migration_script(&migration1, "");
+
+    let execution1 = Execution {
+        key: key("20250103_140520"),
+        applied_rank: 1,
+        applied_by: "tester".into(),
+        applied_at: datetime("2025-01-20T09:10:19Z"),
+        checksum: checksum1,
+        execution_time: Duration::from_micros(256),
+    };
+
+    insert_migration_execution(migration1, execution1, DEFAULT_MIGRATIONS_TABLE, &db)
+        .await
+        .expect("failed to insert migration 1");
+
+    let cmd = surmig().args([
+        "--config-dir",
+        "tests/list_cmd",
+        "--db-address",
+        &db_config.address,
+        "list",
+        "--applied",
+        "--up",
+        "--down",
+    ]);
+
+    cmd.assert()
+        .code(0)
+        .stdout_eq(file!(
+            "list_cmd/applied_forward_and_backward_migrations_one_applied.stdout"
+        ))
         .stderr_eq("");
 }
