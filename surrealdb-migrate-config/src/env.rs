61d6edcad6f1b0dacfee54395678c6556ca96433
haraldmaida <harald.maida@innoave.com>
2025-06-09 01:32:24 +0200

    refactor: use `fakeenv` crate instead of own implementation of a fake env module

--- surrealdb-migrate-config/src/env.rs
+++ surrealdb-migrate-config/src/env.rs
@@ -1,69 +1,38 @@
-//! inspired by
+//! A fakeable implementation of the `std::env` module.
+//!
+//! Inspired by the
 //! [Testing code that uses environment variables](https://www.reddit.com/r/rust/comments/1jd8sxg/testing_code_that_uses_environment_variables/)
-//! - post on Reddit
+//! post on Reddit.
 
 #[cfg(not(test))]
 pub use std::env::{var, vars};
 
 #[cfg(test)]
-pub use stubbed_vars::{remove_var, set_var, var, vars};
+pub use fake_env::{remove_var, set_var, var, vars};
 
 #[cfg(test)]
-mod stubbed_vars {
+mod fake_env {
+    use fakeenv::{EnvStore, Vars};
     use std::cell::RefCell;
-    use std::collections::HashMap;
     use std::env::VarError;
 
     thread_local! {
-        static ENV_VARS: RefCell<HashMap<String, String>> = RefCell::new(HashMap::new());
+        static ENV_STORE: RefCell<EnvStore> = RefCell::new(EnvStore::fake());
     }
 
     pub fn var(name: &str) -> Result<String, VarError> {
-        ENV_VARS
-            .with(|env| env.borrow().get(name).cloned())
-            .ok_or(VarError::NotPresent)
+        ENV_STORE.with(|env| env.borrow().var(name))
     }
 
     pub fn set_var(name: &str, value: &str) {
-        ENV_VARS.with(|env| env.borrow_mut().insert(name.to_string(), value.to_string()));
+        ENV_STORE.with(|env| env.borrow_mut().set_var(name, value));
     }
 
     pub fn remove_var(name: &str) {
-        ENV_VARS.with(|env| env.borrow_mut().remove(name));
+        ENV_STORE.with(|env| env.borrow_mut().remove_var(name));
     }
 
     pub fn vars() -> Vars {
-        ENV_VARS.with(|env| Vars {
-            values: env
-                .borrow()
-                .iter()
-                .map(|(key, value)| (key.clone(), value.clone()))
-                .collect(),
-            next: 0,
-        })
+        ENV_STORE.with(|env| env.borrow().vars())
     }
-
-    pub struct Vars {
-        values: Vec<(String, String)>,
-        next: usize,
-    }
-
-    impl Iterator for Vars {
-        type Item = (String, String);
-
-        fn next(&mut self) -> Option<Self::Item> {
-            let next = self.values.get(self.next).cloned();
-            if next.is_some() {
-                self.next += 1;
-            }
-            next
-        }
-
-        fn size_hint(&self) -> (usize, Option<usize>) {
-            let remaining = self.values.len() - self.next;
-            (remaining, Some(remaining))
-        }
-    }
-
-    impl ExactSizeIterator for Vars {}
 }
